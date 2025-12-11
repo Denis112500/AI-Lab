bfcf06a0be75c2a473b6f70547b4d5c833670034
Denis <196484913+Denis112500@users.noreply.github.com>
2025-12-11 18:53:35 +0200

    Added:Comments and flow understanding

--- train-02.py
+++ train-02.py
@@ -28,7 +28,7 @@ tokenizer.pad_token = tokenizer.eos_token
 print(f"Loading {model_name}...")
 model = AutoModelForCausalLM.from_pretrained(   # Library 
     model_name,
-    torch_dtype=torch.float16,  # Half Precision method (Uses only half of the VRAM it's supposed to use) In this case 2.5 out of 5
+    dtype=torch.float16,        # Half Precision method (Uses only half of the VRAM it's supposed to use) In this case 2.5 out of 5
     device_map = "auto",        # Find the GPU automatically
     trust_remote_code = True    # The model I use has a custom code and this line let's me run it's special setup files
 )
@@ -36,33 +36,33 @@ print("Model loaded succesfully")
 
 # 2.Preparing Input (Translation)
 messages = [
-    {"role": "user" , "content": "Explain coding to a begginer."}   # Organized in a structured list (USER vs AI)
+    {"role": "user" , "content": "Make a poem about Linda"}   # Organized in a structured list (USER vs AI)
 ]
 
 #   APPLY_CHAT_TEMPLATE : Converts the list we gave above into a string that the model understands
 #   It adds special flags like <|user|> or <|assistant|>
 chat_text = tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
 
-#   tokenizer(chat_text, return_tensors = "pt").to(model.device) "Translator" -> Turns the final string into a list of numbers (Tensors) that the GPU can process (The rectangles)
+#   tokenizer(chat_text, return_tensors = "pt").to(model.device) "Translator" -> Turns the tagged string into a list of numbers that the GPU can process (Tensors)/(The Rectangles)
 #   [101, 202]
 #   [303, 000] -> This is a Tensor
-model_inputs = tokenizer(chat_text, return_tensors = "pt").to(model.device)     # pt => PyTorch (It basically takes the lists made by the tokenizer and puts them in a block to create Tensors)
+model_inputs = tokenizer(chat_text, return_tensors = "pt").to(model.device)     # pt => PyTorch (It takes the tagged strings made by the tokenizer and puts them in a block to create Tensors)
 
 print("Generating response... (This might take a few seconds)")
 
 generated_ids = model.generate(
-    model_inputs.input_ids,
-    max_new_tokens = 200,
-    do_sample = True,
-    temperature = 0.7,
-    use_cache = False
+    model_inputs.input_ids,     # "model_inputs" is the container of the tensor and "input_ids" are the integers inside the tensor [<-Tensor [101]<-Integer ]
+    max_new_tokens = 200,       # Max of words that the model can generate
+    do_sample = True,           # Allows the model to be creative
+    temperature = 0.7,          # Controls how creative the model can be | To low => robotic/factual responses | To High => Total gibberish   //Tested\\
+    use_cache = False           # Specific error fix for this model (MiniCPM)
 )
 
 response = [
-    tokenizer.decode(output_ids, skip_special_tokens = True)
-    for output_ids in generated_ids
+    tokenizer.decode(output_ids, skip_special_tokens = True)    #The model outputs TokenIDs (numbers) and translates them back into human language / skip_special_tokens = True , Hides internal flags (</s> <|> etc..)
+    for output_ids in generated_ids         # The loop takes out a piece [output_ids(numbers)] from the container (generated_ids) and sends them to the decoder
 ]
 
 print("-" * 30)
-print(response[0])
+print(response[0])      # Printing final response
 print("-" * 30)
